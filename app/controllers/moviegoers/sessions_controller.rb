b05e0089c8a18414ab89070e28fc775be525c546
Napawan Nakasuwan <97944321+ampamp1a@users.noreply.github.com>
2023-09-25 12:08:26 +0700

    Update sessions_controller.rb

--- app/controllers/moviegoers/sessions_controller.rb
+++ app/controllers/moviegoers/sessions_controller.rb
@@ -3,10 +3,10 @@
 class Moviegoers::SessionsController < Devise::SessionsController
   def after_sign_out_path_for(_resource_or_scope)
       new_moviegoer_session_path
-    end
-    def after_sign_in_path_for(resource_or_scope)
+  end
+  def after_sign_in_path_for(resource_or_scope)
       stored_location_for(resource_or_scope) || root_path
-    end
+  end
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
